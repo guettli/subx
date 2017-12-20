32b09dbeaab6321ad07274e2444a141b2066568f
Bumpversion after CI <bumpversion-after-ci@example.com>
2017-12-20 13:58:58 +0000

    Bump version: 2017.10.0 -> 2017.11.0 [ci skip]

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
 
     # Updated via travisd: https://travis-ci.org/guettli/subx
     # See .travis.yml
-    version='2017.10.0',
+    version='2017.11.0',
 
     description='subx: Datastructure SubprocessResult',
     long_description=long_description,
