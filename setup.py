f4071f41a5b78deb5b5edfb5b00627a8c39e9887
Thomas Guettler <tguettler@tbz-pariv.de>
2018-08-13 15:29:14 +0200

    dummy change to trigger ci.

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
