408ce6885a7bec2dc6d137c04a53eff4ed3c7a7f
Thomas Guettler <tguettler@tbz-pariv.de>
2019-04-01 16:11:10 +0200

    py2 and py3

--- subx/__init__.py
+++ subx/__init__.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, division, unicode_literals, print_function
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 import logging
 import os
+
+import six
 
 try:
     import subprocess32 as subprocess
@@ -21,19 +23,16 @@ except ImportError:
     from subprocess import CalledProcessError
 
 try:
-  basestring
+    basestring
 except NameError:
-  basestring = str
+    basestring = str
 
+try:
+    unicode
+except NameError:
+    unicode = str
 
 logger = logging.getLogger(__name__)
-
-
-def repr_like_python3(something):
-    result = repr(something)
-    if result.startswith("u'") or result.startswith('u"'):
-        return result[1:]
-    return result
 
 
 def call(cmd, input=None, assert_zero_exit_status=True, warn_on_non_zero_exist_status=False, **kwargs):
@@ -57,46 +56,57 @@ def call(cmd, input=None, assert_zero_exit_status=True, warn_on_non_zero_exist_s
 
 
 class SubprocessResult(object):
-    def __init__(self, cmd, ret, stdout='', stderr=''):
+    def __init__(self, cmd, ret, stdout=b'', stderr=b''):
         self.cmd = cmd
         self.ret = ret
+        if isinstance(stdout, unicode):
+            stdout = stdout.encode('ascii')
+        if isinstance(stderr, unicode):
+            stderr = stderr.encode('ascii')
+
         self.stdout = stdout
         self.stderr = stderr
 
     max_head_size = 4000
 
     def __repr__(self):
-        return b'<{} cmd={} ret={} stdout={} stderr={}>'.format(self.__class__.__name__,
-                                                                repr_like_python3(self.cmd_for_copy_and_paste),
+        byte_prefix = ''
+        if six.PY2:
+            byte_prefix='b'
+
+        return b'<{} cmd={}{} ret={} stdout={}{} stderr={}{}>'.format(self.__class__.__name__,
+                                                                byte_prefix, self.cmd_for_copy_and_paste,
                                                                 self.ret,
-                                                                repr_like_python3(self.head_of_string(self.stdout)),
-                                                                repr_like_python3(self.head_of_string(self.stderr)))
+                                                                byte_prefix, repr(self.head_of_string(self.stdout)),
+                                                                byte_prefix, repr(self.head_of_string(self.stderr)))
 
     @property
     def cmd_for_copy_and_paste(self):
         ret = []
         for item in self.cmd:
             if b' ' in item:
-                item = '"{}"'.format(item)
+                item = b'"{}"'.format(item)
             ret.append(item)
-        return b' '.join(ret)
+        ret = b' '.join(ret)
+        return b"'{}'".format(ret)
 
     @classmethod
     def head_of_string(cls, stdout, max_head_size=None):
+        assert isinstance(stdout, bytes)
         if not max_head_size:
             max_head_size = cls.max_head_size
         stdout = stdout.strip()
         if len(stdout) < max_head_size:
             return stdout
-        return '%s ... [cut]' % stdout[:max_head_size]
+        return b'%s ... [cut]' % stdout[:max_head_size]
 
     @classmethod
     def call(cls, cmd, input=None, **kwargs):
         if is_subprocess32:
             kwargs['start_new_session'] = kwargs.get('start_new_session',
                                                      True)  # change default. We want sudo to fail, not to read a password from /dev/tty
-        kwargs.setdefault(b'bufsize', -1)
-        timeout = kwargs.pop(b'timeout', None)
+        kwargs.setdefault('bufsize', -1)
+        timeout = kwargs.pop('timeout', None)
         if input:
             stdin = subprocess.PIPE
         else:
@@ -112,14 +122,17 @@ class SubprocessError(CalledProcessError):
     def __init__(self, subprocess_result):
         super(SubprocessError, self).__init__(subprocess_result.ret, subprocess_result.cmd,
                                               SubprocessResult.head_of_string(subprocess_result.stdout))
-        self.stderr = subprocess_result.stderr
+        self.stderr = SubprocessResult.head_of_string(subprocess_result.stderr)
 
     def __str__(self):
-        return b'%s: stdout=%r stderr=%r' % (super(SubprocessError, self).__str__(), self.output, self.stderr)
+        byte_prefix = ''
+        if six.PY2:
+            byte_prefix='b'
+        return '{}: stdout={}{} stderr={}{}'.format(super(SubprocessError, self).__str__(), byte_prefix, repr(self.output), byte_prefix, repr(self.stderr))
 
 
 def handle_subprocess_pipe_with_timeout(pipe, timeout, input=None):
-    if (not input is None) and not pipe.stdin:
+    if (input is not None) and not pipe.stdin:
         raise Exception('pipe.stdin must be a stream if you pass in input')
     stdout = ''
     stderr = ''
@@ -137,4 +150,4 @@ def handle_subprocess_pipe_with_timeout(pipe, timeout, input=None):
         except subprocess.TimeoutExpired:
             pass
         raise subprocess.TimeoutExpired(pipe.args, timeout, 'stdout: %r stderr: %r' % (stdout, stderr))
-    return (stdout, stderr)
+    return stdout, stderr
--- subx/tests/test_subx.py
+++ subx/tests/test_subx.py
@@ -1,17 +1,19 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, division, unicode_literals, print_function
+from __future__ import absolute_import, division, print_function, unicode_literals
 
-import unittest
-import mock
 import subprocess
+import unittest
+
+import mock
+import six
 import subx
 
 
 class Test(unittest.TestCase):
     def test_call(self):
         subprocess_result = subx.call(['echo', 'foo'])
-        self.assertEqual('foo\n', subprocess_result.stdout)
-        self.assertEqual('', subprocess_result.stderr)
+        self.assertEqual(b'foo\n', subprocess_result.stdout)
+        self.assertEqual(b'', subprocess_result.stderr)
         self.assertEqual(0, subprocess_result.ret)
 
     def test_subprocess_result_call__command_which_does_not_exist(self):
@@ -19,29 +21,29 @@ class Test(unittest.TestCase):
 
     def test_subprocess_result_call__read_stdout(self):
         result = subx.SubprocessResult.call(['cat', '/etc/fstab'])
-        self.assertEqual(open('/etc/fstab').read(), result.stdout)
+        self.assertEqual(open('/etc/fstab', 'rb').read(), result.stdout)
         self.assertEqual(0, result.ret)
-        self.assertEqual('', result.stderr)
+        self.assertEqual(b'', result.stderr)
         self.assertEqual(['cat', '/etc/fstab'], result.cmd)
 
     def test_subprocess_result_call__read_sterr(self):
         result = subx.SubprocessResult.call(['cat', '/file/which/does/not/exist'], env=dict(LANG='C'))
-        self.assertEqual('', result.stdout)
+        self.assertEqual(b'', result.stdout)
         self.assertEqual(1, result.ret)
-        self.assertEqual('cat: /file/which/does/not/exist: No such file or directory\n', result.stderr)
+        self.assertEqual(b'cat: /file/which/does/not/exist: No such file or directory\n', result.stderr)
 
     def test_subprocess_result_call__read_sterr_and_stdout(self):
         result = subx.SubprocessResult.call(['cat', '/etc/fstab', '/file/which/does/not/exist'],
-                                                       env=dict(LANG='C'))
-        self.assertEqual(open('/etc/fstab').read(), result.stdout)
+                                            env=dict(LANG='C'))
+        self.assertEqual(open('/etc/fstab', 'rb').read(), result.stdout)
         self.assertEqual(1, result.ret)
-        self.assertEqual('cat: /file/which/does/not/exist: No such file or directory\n', result.stderr)
+        self.assertEqual(b'cat: /file/which/does/not/exist: No such file or directory\n', result.stderr)
 
     def test_assert_zero_exit_status__ok(self):
         result = subx.call(['cat', '/etc/fstab'], assert_zero_exit_status=True)
-        self.assertEqual(open('/etc/fstab').read(), result.stdout)
+        self.assertEqual(open('/etc/fstab', 'rb').read(), result.stdout)
         self.assertEqual(0, result.ret)
-        self.assertEqual('', result.stderr)
+        self.assertEqual(b'', result.stderr)
         self.assertEqual(['cat', '/etc/fstab'], result.cmd)
 
     def test_assert_zero_exit_status__non_zero(self):
@@ -52,16 +54,16 @@ class Test(unittest.TestCase):
             self.assertEqual(['cat', '/file/which/does/not/exist'], exc.cmd)
             self.assertEqual(1, exc.returncode)
             self.assertEqual(
-                "Command '['cat', '/file/which/does/not/exist']' returned non-zero exit status 1: stdout='' stderr='cat: /file/which/does/not/exist: No such file or directory\\n'",
-                str(exc).replace("u\x27", "'").replace('status 1.:', 'status 1:'))
+                "Command '['cat', '/file/which/does/not/exist']' returned non-zero exit status 1: stdout=b'' stderr=b'cat: /file/which/does/not/exist: No such file or directory'",
+                str(exc).replace("u'", "'").replace('status 1.:', 'status 1:'))
         else:
             raise AssertionError()
 
     def test_warn_on_non_zero_exist_status__ok(self):
         result = subx.call(['cat', '/etc/fstab'], warn_on_non_zero_exist_status=True)
-        self.assertEqual(open('/etc/fstab').read(), result.stdout)
+        self.assertEqual(open('/etc/fstab', 'rb').read(), result.stdout)
         self.assertEqual(0, result.ret)
-        self.assertEqual('', result.stderr)
+        self.assertEqual(b'', result.stderr)
         self.assertEqual(['cat', '/etc/fstab'], result.cmd)
 
     def test_warn_on_non_zero_exist_status__non_zero(self):
@@ -70,43 +72,45 @@ class Test(unittest.TestCase):
             result = subx.call(
                 ['cat', '/file/which/does/not/exist'], assert_zero_exit_status=False, warn_on_non_zero_exist_status=True,
                 env=dict(LANG='C'))
-        self.assertEqual([u"subprocess failed <SubprocessResult cmd='cat /file/which/does/not/exist' ret=1 stdout='' stderr='cat: /file/which/does/not/exist: No such file or directory'>"],
+        self.assertEqual([
+                "subprocess failed <SubprocessResult cmd=b'cat /file/which/does/not/exist' ret=1 stdout=b'' stderr=b'cat: /file/which/does/not/exist: No such file or directory'>"],
             logs)
         self.assertEqual(1, result.ret)
 
-
     def test_subprocess_result__repr(self):
-        result = subx.SubprocessResult(cmd=['dummy'], ret=1, stdout='my-stdout', stderr='my-stderr')
-        self.assertEqual("<SubprocessResult cmd='dummy' ret=1 stdout='my-stdout' stderr='my-stderr'>", repr(result))
+        result = subx.SubprocessResult(cmd=['dummy'], ret=1, stdout=b'my-stdout', stderr=b'my-stderr')
+        self.assertEqual("<SubprocessResult cmd=b'dummy' ret=1 stdout=b'my-stdout' stderr=b'my-stderr'>", repr(result))
 
     def test_call_with_input(self):
-        result = subx.call(['cat'], input='foo', timeout=1)
-        self.assertEqual("<SubprocessResult cmd='cat' ret=0 stdout='foo' stderr=''>", repr(result))
+        result = subx.call(['cat'], input=b'foo', timeout=1)
+        self.assertEqual("<SubprocessResult cmd=b'cat' ret=0 stdout=b'foo' stderr=b''>", repr(result))
 
     def test_repr_of_subprocess_result_is_7bit_ascii__unicode(self):
-        self.assertEqual(r"<SubprocessResult cmd='\xe4' ret=1 stdout='\xf6' stderr='\xfc'>",
-                         repr(subx.SubprocessResult(['ä'], 1, stdout='ö', stderr='ü')))
+        result = repr(subx.SubprocessResult([b'ä'], 1, stdout=b'ö', stderr=b'ü'))
+        self.assertEqual(b"<SubprocessResult cmd=b'\xc3\xa4' ret=1 stdout=b'\\xc3\\xb6' stderr=b'\\xc3\\xbc'>", result)
 
 
     def test_repr_of_subprocess_result_is_7bit_ascii__bytestring(self):
-        self.assertEqual("<SubprocessResult cmd='\\xc3\\xa4' ret=1 stdout='\\xc3\\xb6' stderr='\\xc3\\xbc'>",
-                         repr(subx.SubprocessResult([b'ä'], 1, stdout=b'ö', stderr=b'ü')))
+        stdout = 'ö'.encode('utf8')
+        stderr = 'ü'.encode('utf8')
+        self.assertEqual(b"<SubprocessResult cmd='ä' ret=1 stdout='\\xc3\\xb6' stderr='\\xc3\\xbc'>",
+                         repr(subx.SubprocessResult([b'ä'], 1, stdout=stdout, stderr=stderr)).replace(b"b'", b"'"))
 
     def test_sudo_password_prompt_does_not_wait_for_ever(self):
         result = subx.call(['cat', '/dev/tty'], assert_zero_exit_status=False, timeout=10,
                            env=dict(LANG='C'))
         self.assertEqual(1, result.ret)
-        self.assertIn('No such device or address', result.stderr)
+        self.assertIn(b'No such device or address', result.stderr)
 
     def test_redirect_stderr_to_stdout(self):
         result = subx.call(['cat', '/does/not/exist', '/etc/fstab'],
                            stderr=subprocess.STDOUT, assert_zero_exit_status=False, env=dict(LANG='C'))
         self.assertFalse(result.stderr)
-        self.assertIn('cat: /does/not/exist: No such file or directory', result.stdout)
-    
+        self.assertIn(b'cat: /does/not/exist: No such file or directory', result.stdout)
+
     def test_subx_called_with_incorrect__list_was_ommitted(self):
         self.assertRaises(ValueError, subx.call, 'python', 'setup.py')
 
     def test_subx_with_shell_is_true(self):
-        result = subx.call(['cat'], input='foo', shell=True)
-        self.assertEqual("<SubprocessResult cmd='cat' ret=0 stdout='foo' stderr=''>", repr(result))
+        result = subx.call(['cat'], input=b'foo', shell=True)
+        self.assertEqual("<SubprocessResult cmd=b'cat' ret=0 stdout=b'foo' stderr=b''>", repr(result))
