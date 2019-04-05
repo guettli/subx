47e5121cecaccf6ba01e1fbc90f6ceb0a25ffe8c
Thomas Guettler <tguettler@tbz-pariv.de>
2019-04-05 14:16:01 +0200

    removed py 3.4 3.5

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 
     packages=setuptools.find_packages(),
